e506c4b3d4b47700bf7362fdcf4b91657cb5fed9
pshreyasrao <80516377+pshreyasrao@users.noreply.github.com>
2021-03-12 20:16:54 +0530

    Update wCat.js

--- wCat.js
+++ wCat.js
@@ -1,26 +1,20 @@
-//Importing libraries from commands folder with dependency scripts
 let bigLineBreaks = require("./commands/bigLineBreaks");
 let defaultShowData = require("./commands/defaultShowData");
 let giveNumbers = require("./commands/giveNumbers");
 let nonEmptyNumber = require("./commands/nonEmptyNumber");
-
-//Taking user input
 let input = process.argv.slice(2);
 
 let cmdArgument = input[0];
 
-//Checking user's input / arugment while running the script
 if (cmdArgument == "-s" || cmdArgument == "-n" || cmdArgument == "-b") {
   let dirPath = input[1];
-
-  //Handling single arguments with Switch Case statements
+  
   if (input.length <= 2) {
     switch (cmdArgument) {
       case "-s":
         bigLineBreaks(dirPath);
         break;
-
-      case "-n":
+     case "-n":
         giveNumbers(dirPath);
         break;
 
@@ -33,7 +27,7 @@ if (cmdArgument == "-s" || cmdArgument == "-n" || cmdArgument == "-b") {
         break;
     }
   } else {
-    // Handling chained arguments and edge cases
+    
     if (input.length == 3 && input[0] == "-n" && input[1] == "-s") {
       let path = input[2];
       bigLineBreaks(path);
@@ -51,6 +45,5 @@ if (cmdArgument == "-s" || cmdArgument == "-n" || cmdArgument == "-b") {
     }
   }
 } else {
-  // In case of no argument and ONLY path the script shows all content of the files.
   defaultShowData(input);
 }
